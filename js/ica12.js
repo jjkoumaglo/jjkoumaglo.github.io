eabf1445377b0aebeb9faeb28e86b9529cef828a
jjkoumaglo <jjko6602@colorado.edu>
2023-11-15 16:12:36 -0700

    ica12

---
+++ js/ica12.js
@@ -0,0 +1,40 @@
+const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
+const answerBtn = document.querySelector('#js-tweet').addEventListener('click', testFunction);
+let answer = "";
+
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
+
+async function getQuote() {
+    //console.log('Test');
+
+    try {
+        const response = await fetch(endpoint);
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+
+        const json = await response.json();
+        displayQuote(json['question']);
+        answer = json['answer'];
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to catch new quote');
+    }
+}
+
+function displayQuote(question){
+    const questionTxt = document.querySelector('#js-quote-text');
+    questionTxt.textContent = question;
+}
+
+function displayAnswer(answer) {
+    const answerTxt = document.querySelector('#js-answer-text');
+    answerTxt.textContent = ' ';
+}
+
+function testFunction(){
+    console.log("Anwser button clicked")
+}
+
+getQuote();
