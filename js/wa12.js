55d4e58623fab72dfe35fa9c78ed5f9c02b34347
jjkoumaglo <jjko6602@colorado.edu>
2023-11-21 11:32:56 -0700

    wa12

---
+++ js/wa12.js
@@ -0,0 +1,52 @@
+const newBtn = document.querySelector('#js-new-quote')
+newBtn.addEventListener('click', getQuote);
+
+// API endpoint to get response from 
+const endpoint = 'https://icanhazdadjoke.com';
+
+async function getQuote() {
+    //console.log('Test');
+
+    try {
+        const response = await fetch(endpoint,{
+            headers: {
+                "Accept": "application/json"
+            }
+        });
+        // if !response.ok is "if the response ISN'T okay (status code 200)"
+        if (!response.ok) {
+            throw Error(response.statusText);
+        }
+
+        const json = await response.json();
+        console.log(json);
+        
+        displayQuote(json['joke']);
+        changeBG();
+    }
+    catch(err) {
+        console.log(err);
+        alert('Failed to catch new joke');
+    }
+}
+
+function displayQuote(question){
+    const questionTxt = document.querySelector('#js-quote-text');
+    questionTxt.textContent = question;
+}
+
+function changeBG() {
+    function getRandomColor(){
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random()* 16)];
+        }
+        return color;
+    }
+    let color = getRandomColor();
+    newBtn.style.background = color;
+}
+
+
+getQuote();
